414c929d2f3ecfc7092d8aa9641dabbc2c3279a2
s1Develop <sungwon.dev@gmail.com>
2023-08-21 21:32:54 -0700

    setting up react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+// In {}, we can put any js
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
